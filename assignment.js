d3b6b806dbfa0cdf7122bd5ba97f740c108133c3
swastika <swastikapradhan18@gmail.com>
2026-03-02 22:56:16 +0530

    assignment

---
+++ assignment.js
@@ -0,0 +1,23 @@
+// // operators
+// var x=5; var y=10;
+
+// var result=x+y;
+// console.log("a+b="+result);
+// var result=x-y;
+// console.log("a-b="+result);
+// var result=x*y;
+// console.log("a*b="+result);
+// var result=x/y;
+// console.log("a/b="+result);
+// var result=x%y;
+// console.log("a%b="+result);
+
+
+// maximum of three numbers 
+let a=10, b=25, c=15;
+
+let max=(a>b)
+    ?(a>c?a:c)
+    :(b>c?b:c);
+
+console.log("Maximum is: ", max);
