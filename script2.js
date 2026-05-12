afe1ef7bb99189615fa8246fe98271701bb058ee
swastika <swastikapradhan18@gmail.com>
2026-05-12 21:39:34 +0530

    DOM-2

---
+++ script2.js
@@ -0,0 +1,11 @@
+// <!-- <script type="text/javascript">
+//         var grade='A';
+//         document.write("Entering switch block<br/>");
+//         switch (grade){
+//             case 'A':document.write("Good job <br/>");
+//             case 'B':document.write("Pretty good <br/>");
+//             case 'C':document.write("Passed <br/>");
+//             case 'D':document.write("Good job <br/>");
+//             case 'F':document.write("Fail <br/>");
+//         }
+//     </script> -->
