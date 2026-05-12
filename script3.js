6e2db1554658bf085da007acd1e44c89c99f6688
swastika <swastikapradhan18@gmail.com>
2026-05-12 21:37:01 +0530

    DOM

---
+++ script3.js
@@ -0,0 +1,27 @@
+// let x=document.getElementById("demo");
+// x.innerHTML="";
+// let y=document.getElementsByClassName("demo");
+// y[0]=innerHTML="";
+// let z=document.getElementsByTagName("demo");
+// z[0]=innerHTML="";
+// document.querySelector("demo");
+//     document.querySelectorAll("demo");
+
+// let x=document.getElementById("demo").getAttribute("id");
+// let y=document.getElementById("demo").setAttribute("class","democlass");
+
+// let pax=document.SetElementsByTagName("body");
+// let y=document.createElement("h2");
+// pax[0].append(y);
+
+function clickFunction(){
+            alert("Button Clicked!");
+        }
+
+function mouseOver(){
+        alert("Mouse over!")
+    }
+
+function mouseOut(){
+        alert("Mouse left!")
+    }
